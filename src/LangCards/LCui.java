2a29e08cb27570cc859b5d5dd0fb199ecd5f90af
zzzzsssseeee@gmail.com <zzzzsssseeee@gmail.com@529967c5-7b44-af1a-2c98-77d726487031>
2012-07-13 06:44:32 +0000

    menu, dialog;

--- src/LangCards/LCui.java
+++ src/LangCards/LCui.java
@@ -1,11 +1,22 @@
 package LangCards;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class LCui extends JFrame {
+public class LCui extends JFrame
+					implements ActionListener {
+	
 	private JTextField input = new JTextField("Test", 5);
 	private JLabel label = new JLabel();
+	
+	JMenuBar menuBar;
+	JMenu menu, submenu;
+	JMenuItem menuItem;
+	
+	JFileChooser fc = new JFileChooser();
 	
 	public LCui() {
 		setTitle("Language Cards");
@@ -33,12 +44,44 @@ public class LCui extends JFrame {
 			.addComponent(label)
 		);
 		
-		pack();
+		CreateMenu();
 		
+		pack();
 	}
 	
 	public static void main(String[] args) {
 		LCui ui = new LCui();
 		ui.setVisible(true);
+	}
+	
+	private void CreateMenu() {
+		
+		menuBar = new JMenuBar();
+		menu = new JMenu("Set");
+		menuBar.add(menu);
+		
+		menuItem = new JMenuItem("New");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Open");
+		menuItem.addActionListener(this);
+		
+		menu.add(menuItem);
+		
+		this.setJMenuBar(menuBar);
+	}
+
+	// ActionListener
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+        String actionCmd = arg0.getActionCommand();
+        if (actionCmd.equals("New")) {
+        	fc.showDialog(this, "New");
+        } else if (actionCmd.equals("Open")) {
+        	fc.showOpenDialog(this);
+        }
+		
 	}
 }
