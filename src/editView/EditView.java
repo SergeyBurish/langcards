9dae702e53a38ee7cc4db61bff0775522b7f4880
zzzzsssseeee@gmail.com <zzzzsssseeee@gmail.com@529967c5-7b44-af1a-2c98-77d726487031>
2012-07-26 06:13:24 +0000

    NewCard dialog added;

--- src/editView/EditView.java
+++ src/editView/EditView.java
@@ -1,7 +1,8 @@
 package editView;
 
-import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
@@ -11,21 +12,29 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTree;
 import javax.swing.LayoutStyle;
+import javax.swing.table.DefaultTableModel;
+
+import newCardDlg.NewCardDlg;
 
 import LangCards.LCui;
 import cardSet.CardSet;
 
-public class EditView {
+public class EditView implements ActionListener {
 	private CardSet iSet;
 	
-	private JTabbedPane iTabbedPane = new JTabbedPane();
 	private JButton iBtAdd = new JButton("Add");
 	private JButton iBtDel = new JButton("Delete");
 	private JButton iBtEd = new JButton("Edit");
 	
+	private DefaultTableModel iTableModel = new DefaultTableModel();
+	private NewCardDlg iNewCardDlg = new NewCardDlg(null);	
 	
 	public EditView(CardSet set) {
 		iSet = set;
+		
+		iBtAdd.addActionListener(this);
+		iBtDel.addActionListener(this);
+		iBtEd.addActionListener(this);
 	}
 	
 	public void Show() {
@@ -34,39 +43,47 @@ public class EditView {
 		
 		LCui.mainFrame.iContainer.removeAll(); // remove all ui controls
 		
+		JTabbedPane tabbedPane = new JTabbedPane();
+		
 		JPanel panCards = makeCardsPanel();
-		iTabbedPane.addTab("Cards", panCards);
+		tabbedPane.addTab("Cards", panCards);
 		
 		JPanel panSett = makeSettingsPanel();
-		iTabbedPane.addTab("Settings", panSett);
+		tabbedPane.addTab("Settings", panSett);
 		
 		LCui.mainFrame.iLayout.setHorizontalGroup(
 				LCui.mainFrame.iLayout.createSequentialGroup()
-				.addComponent(iTabbedPane)
+				.addComponent(tabbedPane)
 		);
 				
 		LCui.mainFrame.iLayout.setVerticalGroup(
 				LCui.mainFrame.iLayout.createSequentialGroup()
-				.addComponent(iTabbedPane)
+				.addComponent(tabbedPane)
 		);
 		
 		LCui.mainFrame.pack();
 	}
-	    
-    private JPanel makeCardsPanel() {
-        JPanel panel = new JPanel(false);
-        
-        Object[][] cellData = { { "1-1", "1-2" }, { "2-1", "2-2" } };
-        String[] columnNames = { "col1", "col2" };
-        JTable table = new JTable(cellData, columnNames);
-        
-        JScrollPane sp = new JScrollPane(table);
-        
-        GroupLayout layout = new GroupLayout(panel);
-        panel.setLayout(layout);
-        
-        layout.setHorizontalGroup(
-        		layout.createSequentialGroup()
+	
+	private JPanel makeCardsPanel() {
+		JPanel panel = new JPanel(false);
+		
+		JTable table = new JTable(iTableModel);
+		
+		iTableModel.addColumn("Col1");
+		iTableModel.addColumn("Col2");
+		iTableModel.addRow(new Object[] { "v1", "v2" });
+		iTableModel.addRow(new Object[] { "v3" });
+		iTableModel.addRow(new Object[] { "v4", "v555", "v6" });
+		
+		JScrollPane sp = new JScrollPane(table);
+		
+		GroupLayout layout = new GroupLayout(panel);
+		panel.setLayout(layout);
+		
+		layout.setAutoCreateContainerGaps(true);
+		
+		layout.setHorizontalGroup(
+				layout.createSequentialGroup()
 				.addComponent(sp)
 				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
@@ -74,34 +91,51 @@ public class EditView {
 						.addComponent(iBtDel)
 						.addComponent(iBtEd))
 		);
-        
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {iBtAdd, iBtDel, iBtEd});
-				
-        layout.setVerticalGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+		
+		layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {iBtAdd, iBtDel, iBtEd});
+		
+		layout.setVerticalGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 				.addComponent(sp)
 				.addGroup(layout.createSequentialGroup()
 						.addComponent(iBtAdd)
 						.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 						.addComponent(iBtDel)
 						.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-						.addComponent(iBtEd))
+						.addComponent(iBtEd)
+				)
 		);
-
-        return panel;
-    }
-    
-    private JPanel makeSettingsPanel() {
-        JPanel panel = new JPanel(false);
-        
-        JTree tree = new JTree();
-        tree.setAlignmentX(Component.CENTER_ALIGNMENT);
-        tree.setAlignmentY(Component.CENTER_ALIGNMENT);
-        
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(tree);
-        return panel;
-    }
-
-
+		
+		return panel;
+	}
+	
+	private JPanel makeSettingsPanel() {
+		JPanel panel = new JPanel(false);
+		
+		JTree tree = new JTree();
+		
+		panel.setLayout(new GridLayout(1, 1));
+		panel.add(tree);
+		return panel;
+	}
+	
+	// ActionListener
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		Object srcObject = event.getSource();
+		
+		if (srcObject.getClass().getSimpleName().compareTo("JButton") == 0) {
+			JButton srcBt = (JButton)srcObject;
+			
+			String btName = srcBt.getText();
+			
+			if (btName.compareTo("Add") == 0) {
+				iNewCardDlg.setVisible(true);
+				
+				//iTableModel.addRow(new Object[] { btName });
+			} else {
+				iTableModel.addColumn(btName);
+			}
+		}
+	}
 }
