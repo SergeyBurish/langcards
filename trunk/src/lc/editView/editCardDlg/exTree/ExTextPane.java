5f8f49ad05b3a8a1c9f8c1a70f2e53e36af5db2d
zzzzsssseeee@gmail.com <zzzzsssseeee@gmail.com@529967c5-7b44-af1a-2c98-77d726487031>
2012-10-31 11:05:55 +0000

    Size update on multi-line node edit;

--- trunk/src/lc/editView/editCardDlg/exTree/ExTextPane.java
+++ trunk/src/lc/editView/editCardDlg/exTree/ExTextPane.java
@@ -1,14 +1,45 @@
 package lc.editView.editCardDlg.exTree;
 
+import java.awt.Dimension;
+import java.awt.FontMetrics;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 
 import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
 
 public class ExTextPane extends JTextPane {
 	ExTextPaneListener listener;
 	
 	public ExTextPane(ExTextPaneListener tpListener) {
 		listener = tpListener;
+	}
+	
+	public void UpdateSize() {
+		String str = getText();
+		
+		FontMetrics fm = getToolkit().getFontMetrics(getFont());
+		BufferedReader br = new BufferedReader(new StringReader(str));
+		String line;
+		int maxWidth = 0, lines = 0;
+		try {
+			while ((line = br.readLine()) != null) {
+				int width = SwingUtilities.computeStringWidth(fm, line);
+				if (maxWidth < width) {
+					maxWidth = width;
+				}
+				lines++;
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		lines = (lines < 1) ? 1 : lines;
+		int height = fm.getHeight() * lines;
+		Dimension dm = new Dimension(maxWidth + 6, height+6);
+		setSize(dm);
+		setPreferredSize(dm);
 	}
 	
 	@Override
--- trunk/src/lc/editView/editCardDlg/exTree/MultiLineCellEditor.java
+++ trunk/src/lc/editView/editCardDlg/exTree/MultiLineCellEditor.java
@@ -2,6 +2,7 @@ package lc.editView.editCardDlg.exTree;
 
 import java.awt.Component;
 import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.event.CaretEvent;
@@ -9,38 +10,48 @@ import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
+import javax.swing.tree.DefaultTreeModel;
 
 import lc.LCmain;
 
 public class MultiLineCellEditor extends DefaultCellEditor implements DocumentListener, CaretListener, ExTextPaneListener{
 	static ExTextPane iTextPane;
+	DefaultTreeModel iModel;
+	ExTreeNode iNode = null;
 
-	public MultiLineCellEditor() {
+	public MultiLineCellEditor(DefaultTreeModel model) {
 		super(new JTextField());
 		iTextPane = new ExTextPane(this);
 		iTextPane.addCaretListener(this);
 		iTextPane.getDocument().addDocumentListener(this);
+		
+		iModel = model;
+	}
+	
+	private void UpdateSizes() {
+		iTextPane.UpdateSize();
+		
+		if (iModel != null && iNode != null) {
+			iModel.nodeChanged(iNode);
+		}
 	}
 
 	@Override
 	// DocumentListener
 	public void changedUpdate(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		
+		UpdateSizes();
 	}
 
 	@Override
 	// DocumentListener
 	public void insertUpdate(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		
+		UpdateSizes();
 	}
 
 	@Override
 	// DocumentListener
 	public void removeUpdate(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		
+		UpdateSizes();
 	}
 
 	@Override
@@ -61,6 +72,11 @@ public class MultiLineCellEditor extends DefaultCellEditor implements DocumentLi
 	public Component getTreeCellEditorComponent(JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row) {
 		String stringValue = tree.convertValueToText(value, isSelected, expanded, leaf, row, true);
 		iTextPane.setText(stringValue);
+		
+		if (value instanceof ExTreeNode) {
+			iNode = (ExTreeNode)value;
+		}
+		
 		return iTextPane;
 	}
 
