b23e26d52ebdbc6d2b3440b939fca7163dfe4032
zzzzsssseeee@gmail.com <zzzzsssseeee@gmail.com@529967c5-7b44-af1a-2c98-77d726487031>
2015-03-22 18:13:06 +0000

    bug fixing: promt text handling;

--- src/lc/editView/editCardDlg/exTree/ExTreeNode.java
+++ src/lc/editView/editCardDlg/exTree/ExTreeNode.java
@@ -4,6 +4,8 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 public class ExTreeNode extends DefaultMutableTreeNode {
 	private boolean iEditable;
+	private boolean changed;
+
 	public ExTreeNode (Object userObject, boolean editable) {
 		super(userObject);
 		iEditable = editable;
@@ -11,5 +13,16 @@ public class ExTreeNode extends DefaultMutableTreeNode {
 	
 	public boolean isEditable() {
 		return iEditable;
+	}
+
+	public void setChanged(boolean changed) {
+		this.changed = changed;
+	}
+
+	public String getChangedString() {
+		if (changed) {
+			return super.toString();
+		}
+		return "";
 	}
 }
