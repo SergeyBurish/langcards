9155c2ec6b2d2629f40e2e656235f73005aafc18
sergey.burish <sergey.burish@hiq.ru>
2015-10-15 11:22:07 +0300

    Transcription keyboard optimization

--- src/lc/editView/editCardDlg/Keyboard.java
+++ src/lc/editView/editCardDlg/Keyboard.java
@@ -5,46 +5,36 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * Created by sergey.burish on 19.08.2015.
- */
-
 public class Keyboard extends JFrame {
 
 	private JPanel base = new JPanel(new GridLayout(0, 1));
-	private JPanel[] panel;
-	private JButton[][] button;
 	private String[][] key = null;
 
 	KBListener listener;
 
 	public Keyboard(String keys, KBListener listener) {
-
 		String[] keyRows = keys.split("\\n");
 		key = new String[keyRows.length][];
 
 		for (int row = 0; row < keyRows.length; row++) {
 			key[row] = keyRows[row].split("");
 		}
-
 		this.listener = listener;
 	}
 
 	public void init() {
 		setUndecorated(true);
-		panel = new JPanel[6];
 		for (int row = 0; row < key.length; row++) {
-			panel[row] = new JPanel();
-			button = new JButton[20][20];
+			JPanel panel = new JPanel();
 			for (int column = 0; column < key[row].length; column++) {
-				button[row][column] = new JButton(key[row][column]);
-				button[row][column].putClientProperty("column", column);
-				button[row][column].putClientProperty("row", row);
-				button[row][column].putClientProperty("key", key[row][column]);
-				button[row][column].addActionListener(new MyActionListener());
-				panel[row].add(button[row][column]);
+				JButton button = new JButton(key[row][column]);
+				button.putClientProperty("column", column);
+				button.putClientProperty("row", row);
+				button.putClientProperty("key", key[row][column]);
+				button.addActionListener(new MyActionListener());
+				panel.add(button);
 			}
-			base.add(panel[row]);
+			base.add(panel);
 		}
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		add(base);
